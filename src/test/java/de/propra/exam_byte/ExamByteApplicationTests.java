ac693efa131d3072bec9177c27940337553778a9
Nikolas Edinger <nikolas.edinger@gmail.com>
2024-11-29 13:12:34 +0100

    Initialisierung Spring Boot Projekt

---
+++ src/test/java/de/propra/exam_byte/ExamByteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.propra.exam_byte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamByteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
